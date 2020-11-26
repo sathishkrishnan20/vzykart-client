7e3f04361c73c9d303db94e73651dbf1c8464ad7
Sathish Krishnan <sathiskrish20@gmail.com>
2020-11-26 12:56:28 +0530

    build-configs

--- config-overrides.js
+++ config-overrides.js
@@ -15,6 +15,13 @@ module.exports = override(
         path.resolve(__dirname, 'node_modules/react-native-elements'),
         path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
         path.resolve(__dirname, 'node_modules/react-native-ratings'),
+        path.resolve(__dirname, 'node_modules/react-native-gesture-handler'),
+        path.resolve(__dirname, 'node_modules/react-native-otp-textinput'),
+        path.resolve(__dirname, 'node_modules/react-native-picker-select'),
+        path.resolve(__dirname, 'node_modules/react-native-sectioned-multi-select'),
+        path.resolve(__dirname, 'node_modules/react-native-toast-message'),
+
+
         path.resolve(__dirname, 'src'),
     ])
 );
