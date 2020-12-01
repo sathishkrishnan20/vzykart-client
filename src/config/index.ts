858b00db4c1cd1eb04c8833d3745e5f28d11e73a
Sathish Krishnan <sathiskrish20@gmail.com>
2020-12-01 20:28:30 +0530

    logo-changed

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import {Platform, Dimensions} from 'react-native';
 
-export const SERVER_URL = 'http://192.168.1.11:8050';
+export const SERVER_URL = 'http://ec2-52-14-174-212.us-east-2.compute.amazonaws.com:8050';
 export const IMAGE_BASE_URL = SERVER_URL;
 export const RAZORPAY_KEY = 'rzp_test_HoaTilDmiHfZnE';
 
